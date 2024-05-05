4596ed1dc554c36b4534c0ba6805f6cbb265612f
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-05 11:10:50 -0700

    Add player to entities

---
+++ src/entities.ts
@@ -0,0 +1,22 @@
+import { KaboomCtx } from "kaboom";
+import { scale } from "./constants";
+
+export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
+  const player = k.make([
+    k.sprite("assets", { anim: "kirbIdle" }),
+    k.area({ shape: new k.Rect(k.vec2(4, 5.9), 8, 10) }),
+    k.body(),
+    k.pos(posX * scale, posY * scale),
+    k.scale(scale),
+    k.doubleJump(10),
+    k.health(3),
+    k.opacity(1),
+    {
+      speed: 300,
+      direction: "right",
+      isInhaling: false,
+      isFull: false,
+    },
+    "player",
+  ]);
+}
