a758a852829b097847b593d0acb23605558a5dfb
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-06 16:58:58 -0700

    Add inhale logic

--- src/entities.ts
+++ src/entities.ts
@@ -55,4 +55,30 @@ export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
   player.onCollide("exit", () => {
     k.go("level-2");
   });
+
+  const inhaleEffect = k.add([
+    k.sprite("assets", { anim: "kirbInhaleEffect" }),
+    k.pos(),
+    k.scale(scale),
+    k.opacity(0),
+    "inhaleEffect",
+  ]);
+
+  const inhaleZone = player.add([
+    k.area({ shape: new k.Rect(k.vec2(0), 20, 4) }),
+    k.pos(),
+    "inhaleZone",
+  ]);
+
+  inhaleZone.onUpdate(() => {
+    if (player.direction === "left") {
+      inhaleZone.pos = k.vec2(-14, 8);
+      inhaleEffect.pos = k.vec2(player.pos.x - 60, player.pos.y + 0);
+      inhaleEffect.flipX = true;
+      return;
+    }
+    inhaleZone.pos = k.vec2(14, 8);
+    inhaleEffect.pos = k.vec2(player.pos.x + 60, player.pos.y + 0);
+    inhaleEffect.flipX = false;
+  });
 }
