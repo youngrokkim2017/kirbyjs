dfd41d316fb0fab0a4dda1d31aa40f297a61b27a
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-09 20:06:26 -0700

    Add function to inhale enemy

--- src/entities.ts
+++ src/entities.ts
@@ -179,3 +179,29 @@ export function setControls(k: KaboomCtx, player: PlayerGameObj) {
     }
   });
 }
+
+export function makeInhalable(k: KaboomCtx, enemy: GameObj) {
+  enemy.onCollide("inhaleZone", () => {
+    enemy.isInhalable = true;
+  });
+
+  enemy.onCollideEnd("inhaleZone", () => {
+    enemy.isInhalable = false;
+  });
+
+  enemy.onCollide("shootingStar", (shootingStar: GameObj) => {
+    k.destroy(enemy);
+    k.destroy(shootingStar);
+  });
+
+  const playerRef = k.get("player")[0];
+  enemy.onUpdate(() => {
+    if (playerRef.isInhaling && enemy.isInhalable) {
+      if (playerRef.direction === "right") {
+        enemy.move(-800, 0);
+        return;
+      }
+      enemy.move(800, 0);
+    }
+  });
+}
